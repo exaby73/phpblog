d721beeb65ba3465af3071d67f32b8a90a41b82b
Nabeel Parkar <nabeelparkar99@gmail.com>
2020-02-16 20:00:45 +0530

    Added credentials

---
+++ include/credentials.php
@@ -0,0 +1,13 @@
+<?php
+
+// localhost credentials
+// $dbhost = 'localhost';
+// $dbname = 'wbp_blog';
+// $dbuser = 'postgres';
+// $dbpass = '';
+
+// Heroku credentials
+$dbhost = "ec2-184-72-235-159.compute-1.amazonaws.com";
+$dbname = "d4ichldublet8b";
+$dbuser = "cdheqyvmwisrkh";
+$dbpass = "990069b4cf2e8797bf1ee8ba245080aaebf9f6b503328559184385766a359711";
